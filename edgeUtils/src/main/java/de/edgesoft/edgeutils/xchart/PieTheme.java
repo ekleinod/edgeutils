8ecc82d7f98b01911ea494cc3c3e0248336876ef
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-04-02 02:16:01 +0200

    started xchart utils, not in good shape yet

---
+++ edgeUtils/src/main/java/de/edgesoft/edgeutils/xchart/PieTheme.java
@@ -0,0 +1,44 @@
+package de.edgesoft.edgeutils.xchart;
+
+import org.knowm.xchart.PieChart;
+import org.knowm.xchart.style.Theme;
+
+
+
+/**
+ * Class providing xchart {@link Theme} for {@link PieChart}.
+ *
+ * ## Legal stuff
+ *
+ * Copyright 2010-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
+ *
+ * This file is part of edgeUtils.
+ *
+ * edgeUtils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * edgeUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with edgeUtils.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Ekkart Kleinod
+ * @version 0.14.0
+ * @since 0.14.0
+ */
+public class PieTheme extends BaseTheme {
+
+
+	@Override
+	public boolean isLegendVisible() {
+		return false;
+	}
+
+}
+
+/* EOF */
