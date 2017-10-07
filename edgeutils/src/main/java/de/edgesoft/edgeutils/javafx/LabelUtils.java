cb826800fb4d0e1f3493a6054db5a7dc8b3e70f5
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-10-07 19:32:17 +0200

    added utilities for labels (setting text from string property)

---
+++ edgeutils/src/main/java/de/edgesoft/edgeutils/javafx/LabelUtils.java
@@ -0,0 +1,52 @@
+package de.edgesoft.edgeutils.javafx;
+
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Label;
+
+
+/**
+ * Utilities for {@link Label}.
+ *
+ * ## Legal stuff
+ *
+ * Copyright 2010-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
+ *
+ * This file is part of edgeUtils.
+ *
+ * edgeUtils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * edgeUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with edgeUtils.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Ekkart Kleinod
+ * @version 0.11.0
+ * @since 0.11.0
+ */
+public class LabelUtils {
+
+	/**
+	 * Sets text from {@link StringProperty}.
+	 *
+	 * @param theLabel label to set text
+	 * @param theText text to set
+	 */
+	public static void setText(Label theLabel, final StringProperty theText) {
+
+		theLabel.setText(
+				(theText == null) ?
+						null :
+						theText.getValue());
+
+	}
+
+}
+
+/* EOF */
